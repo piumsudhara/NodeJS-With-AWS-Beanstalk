781378dd9ee296ea178dc0176538b42a1c6d1ca6
Pium Sudhara <pium.karunasena@gmail.com>
2021-05-31 03:07:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.get("/", (req, res) => res.json("Hello From AWS Beanstalk..."));
+app.get("/", (req, res) => res.json("Hello From AWS"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
